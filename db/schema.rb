e96393d6c6004986f73c7e444820af30ea8f04b2
mikitaro-27 <matt.diak@gmail.com>
2022-02-09 16:00:36 +0000

    recreated User model and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_221819) do
+ActiveRecord::Schema.define(version: 2022_02_09_155040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2022_02_07_221819) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "title"
-    t.string "string"
-    t.text "body"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
