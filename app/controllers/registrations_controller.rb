26b7ada45ec3c31f9075d5f3e57fd0c66fec506c
Cristian <cristian.christensen@gmail.com>
2022-02-10 12:26:43 +0100

    Added session controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,20 @@
 class RegistrationsController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id #Do we need to use @ here??
+      redirect_to root_path, notice: 'User created successfully'
+    else
+      render :new
+    end
   end
 
   def new
+    @user = User.new # How does this work?
   end
 
+  private
   def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
