c4f72f1aef5b3cd2a484ab66857a3c2e4f667d33
Cristian <cristian.christensen@gmail.com>
2022-02-11 12:19:20 +0100

    Refactored signup/login tests

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -2,27 +2,15 @@ require 'rails_helper'
 
 feature 'Log in' do
   scenario 'user logs in' do
-    visit '/'
-    p current_path
     # we need to have a registered user
-
-    click_button "Sign up"
-    expect(page).to have_content 'Registrations#new'
-    # user takes name, email, password
-    fill_in "Name", with: "Mat"
-    fill_in "Email", with: "matt@matt.com"
-    fill_in "Password", with: "Abc123"
-    find('input[name="commit"]').click
-
-    click_button 'Log out'
+    User.create(name: "Matt", email: "matt@matt.com", password: "Abc123")
+    visit '/'
     
-    # User.create(name: 'Mat', email: 'matt@g.com', password: '12345')
     click_button 'Log in'
-    p 'After click log in:', current_path
     fill_in "email", with: 'matt@matt.com'
     fill_in "password", with: 'Abc123'
-    click_button 'Log in'
-    p 'AFTER AFTER click log in:', current_path
+    find("input[type=submit][value='Login']").click
+    
     expect(current_path).to eq '/'
     expect(page).to have_content 'Logged in successfully'
     expect(page).not_to have_button 'Sign up'
