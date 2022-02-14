b2b59678f981d546e37490dc4fbcb234f9c74207
Josh <joshlim092@gmail.com>
2022-02-14 19:04:00 +0000

    user can sign up spec with pending task to add logged in as

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,19 +4,22 @@ feature 'Sign up' do
   scenario 'user signs up' do
     visit "/"
     
-    click_button "Sign up"
-    expect(page).to have_content 'Registrations#new'
+    click_link "Sign up"
+    expect(page).to have_content 'Sign up'
     
     # user takes name, email, password
-    fill_in "Name", with: "Mat"
-    fill_in "Email", with: "matt@matt.com"
-    fill_in "Password", with: "Abc123"
-    find('input[name="commit"]').click
-    expect(page).to have_content 'User created successfully'
+    fill_in "Name", with: "Josh"
+    fill_in "Email", with: "Josh@gmail.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(page).to have_button 'Log Out'
+    # expect(page).to have_content 'Logged in as: Josh@gmail.com'
     expect(page).not_to have_button 'Sign up'
-    expect(page).to have_button 'Log out'
     expect(page).not_to have_button 'Log in'
-    expect(page).to have_content 'Logged in as: matt@matt.com'
+    
 
   end
 
