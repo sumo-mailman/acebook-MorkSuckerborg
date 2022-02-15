a35b5caa73414fdc3ba1a4992bed243664c3818a
Cristian <cristian.christensen@gmail.com>
2022-02-15 13:17:52 +0100

    User can edit images in post

--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can edit posts" do
+  scenario "Can edit message" do
     sign_up
     submit_post
     click_link 
@@ -11,6 +11,26 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to_not have_content("Hello, world!")
     expect(page).to have_content("Edited message here!")
   end
+
+  scenario "Can edit photo" do
+    sign_up
+    submit_post
+    click_link 
+    click_link "Edit"
+
+    attach_file('post-image', './spec/fixtures/test-image2.png')
+    click_button "Submit"
+
+    posts = page.all(".post")
+    expect(posts.length).to eq 1
+    expect(posts.first).to have_content("Hello, world!")
+    first_post_image = page.find('#post-image-0')['src']
+    expect(first_post_image).to match /.*\/test-image2.png/
+
+  end
+
+  
+
 end
 
 def sign_up
@@ -23,5 +43,6 @@ end
 
 def submit_post
   fill_in "Message", with: "Hello, world!"
+  attach_file('post-image', './spec/fixtures/test-image.jpeg')
   click_button "Submit"
 end
